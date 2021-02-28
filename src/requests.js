b748de7ec4d097321af73431812601cfb2601a6e
ghost <fayyad_r22@yahoo.com>
2021-02-28 19:57:03 +0100

    add some fetch api

--- src/requests.js
+++ src/requests.js
@@ -4,7 +4,13 @@ const request = {
   fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
   fetchTopRated: `/moive/top_rated?api_key=${APIKEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`
+  fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+
+  fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+
+  fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+
+  fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`
 };
 
 export default request;
